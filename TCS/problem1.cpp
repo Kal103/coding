edc57168c8f93a11ce6aa1f53c61ecef40da0d28
Akash671 <akashsaini454545@gmail.com>
2021-08-31 02:52:49 +0530

    TCS coding problem

--- TCS/problem1.cpp
+++ TCS/problem1.cpp
@@ -1,50 +1,15 @@
 /* author : @akash kumar 
    github : https://www.github.com/Akash671
 */
-problem statement link - https://prepinsta.com/tcs-coding-question-1/
+
 /* 
 problem is:-
-
+problem statement link - https://prepinsta.com/tcs-coding-question-1/
 */
 
-#include<bits/stdc++.h>
+#include<iostream>
 using namespace std;
 
-#define ll long long int
-#define MOD 1000000007
-#define pb push_back
-#define ld long double
-typedef pair<int, int> PII;
-typedef vector<int> VI;
-typedef vector<string> VS;
-typedef vector<PII> VII;
-typedef vector<VI> VVI;
-typedef map<int,int> MPII;
-typedef set<int> SETI;
-typedef multiset<int> MSETI;
-typedef long int int32;
-typedef unsigned long int uint32;
-typedef long long int int64;
-typedef unsigned long long int  uint64;
-
-
-/* calculate power in O(Logn) time 
-ll power(ll base , ll n)
-{
-	// initialize ans = 1;
-	ll ans = 1;
-	while(n>0)
-	{
-		// whenever power is odd , club it with the ans
-		if(n%2)ans = (ans*base)%MOD;
- 
-		base = (base * base)%MOD; // make base square each time
- 
-		n>>=1;  // each time divide the power by 2
-	}
-	return ans;
-}
-*/
 
 void solve()
 {
