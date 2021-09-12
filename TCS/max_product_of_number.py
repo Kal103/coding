a0e8377bd026e8b62510a7201f41137ba3809229
Akash671 <akashsaini454545@gmail.com>
2021-09-13 01:47:47 +0530

    add by Akash671

---
+++ TCS/max_product_of_number.py
@@ -0,0 +1,25 @@
+for _ in range(int(input())):
+   def power(x,n):
+      res=1
+      while(n):
+         if n & 1:
+            res=res*x
+         x=x*x
+         n>>=1
+      return res
+   def ans(n):
+      tmp_ans=0
+      if n==2:
+        return 1
+      elif n==3:
+        return 2
+      elif n%3==0:
+        tmp_ans=power(3,int(n/3))
+        return tmp_ans
+      elif n%3==1:
+        tmp_ans=2*2*power(3,int(n/3)-1)
+        return tmp_ans
+      elif n%3==2:
+        tmp_ans=2*power(3,int(n/3))
+        return tmp_ans
+   print(ans(int(input())))
